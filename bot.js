2a6f1a3530764a161db51f6c6df60d8ed1f469f8
KaRiLaA <kiril.andreyev@gmail.com>
2020-10-04 15:38:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "*";
+
+rolename = 'ROLENAME'
+bot_token = 'BOT_TOKEN'
 
 client.on("ready", ()=>{
     console.log(`${client.user.tag} connected to Discord`);
 });
 
 client.on("guildMemberAdd", member =>{
-    member.roles.add(member.guild.roles.cache.find(role => role.name == "ROLENAME"), "auto added.");
+    member.roles.add(member.guild.roles.cache.find(role => role.name == rolename), "auto added.");
 	console.log(`Role added to ${member.user.tag}`);
   })
-client.login("BOT_TOKEN")
+client.login(bot_token)
